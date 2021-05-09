d99e342be9ec818c58b5bd314a02652856f7c51e
pilpod <giacomo.corso@gmail.com>
2021-05-09 13:35:39 +0200

    addind cards with movies

--- src/Views/homepage.php
+++ src/Views/homepage.php
@@ -2,7 +2,28 @@
 <?php include('layout/header.php') ?>
 
     <main class="container mt-4">
-        <h4>Product List</h4>    
+        <h4>Product List</h4>
+        
+        <div class="row gx-5">
+            <div class="card" style="width: 18rem;">
+                <img src="public/img/posters/chaos_walking-930186574-large.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Chaos Walking</h5>
+                    <p class="card-text">En Prentisstown, Todd ha crecido creyendo que la "Masilla" fue la responsable de liberar un germen que asesinó a todas las mujeres y contagió con el "ruido" al resto de los hombres.</p>
+                    <a href="#" class="btn btn-primary mt-2">Add to cart</a>
+                </div>
+            </div>
+    
+            <div class="card" style="width: 18rem;">
+                <img src="public/img/posters/godzilla_vs_kong-370227109-large.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Chaos Walking</h5>
+                    <p class="card-text">En Prentisstown, Todd ha crecido creyendo que la "Masilla" fue la responsable de liberar un germen que asesinó a todas las mujeres y contagió con el "ruido" al resto de los hombres.</p>
+                    <a href="#" class="btn btn-primary mt-2">Add to cart</a>
+                </div>
+            </div>
+        </div>
+
     </main>
 
 <?php include('layout/footer.php') ?>
