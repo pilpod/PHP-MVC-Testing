9a2b5f5c5268761a469270ab49654f0772b62937
pilpod <giacomo.corso@gmail.com>
2021-07-21 11:20:38 +0200

    Movie model created pending connect database

---
+++ src/Models/MovieModel.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Models;
+
+class MovieModel {
+
+    private ?int $id;
+    private string $img;
+    private string $title;
+    private string $description;
+
+    public function __construct(string $title, string $description, string $img, ?int $id )
+    {
+        $this->id = $id;
+        $this->img = $img;
+        $this->title = $title;
+        $this->description = $description;
+    }
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of img
+     */ 
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * Set the value of img
+     *
+     * @return  self
+     */ 
+    public function setImg($img)
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
